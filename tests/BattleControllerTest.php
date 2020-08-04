05891d22682c355f2edefe44fdda1b32973caa89
Ogawa Nobuya <pigretyasushi@gmail.com>
2020-08-04 09:20:44 +0900

    指摘内容修正

--- tests/BattleControllerTest.php
+++ tests/BattleControllerTest.php
@@ -6,23 +6,50 @@ use App\Entity\User;
 use App\Repository\CpuResultLogRepository;
 use App\Repository\UserRepository;
 use DateTime;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
 use Exception;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class BattleControllerTest extends WebTestCase
 {
+    /** @var KernelBrowser */
+    private $client;
+    /** @var UserRepository */
+    private $userRepository;
+    /** @var CpuResultLogRepository */
+    private $cpuResultLogRepository;
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function setUp()
+    {
+        $this->client = static::createClient();
+        $this->userRepository = static::$container->get(UserRepository::class);
+        $this->cpuResultLogRepository = static::$container->get(CpuResultLogRepository::class);
+        $user = new User();
+        $user->setName('battleTest');
+        $user->setPoint(100);
+        $this->userRepository->add($user);
+    }
+
+    /**
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
     public function testBattleAble(): void
     {
-        $client = static::createClient();
-        $userRepository = static::$container->get(UserRepository::class);
         $user = new User();
         $user->setName('test');
         $user->setPoint(5);
-        $userRepository->add($user);
+        $this->userRepository->add($user);
         /** @var User $testUser */
-        $testUser = $userRepository->findOneBy(['name' => 'test']);
+        $testUser = $this->userRepository->findOneBy(['name' => 'test']);
 
-        $crawler = $client->request('GET', '/battle/'.$testUser->getId());
+        $crawler = $this->client->request('GET', '/battle/'.$testUser->getId());
         // 10pt未満のプレイヤーのじゃんけん画面にはpが5つ、10pt以上は4つ
         $this->assertSame(5, $crawler->filter('p')->count());
     }
@@ -30,12 +57,12 @@ class BattleControllerTest extends WebTestCase
     public function testChoiceNullAble(): void
     {
         $nullAble = true;
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/battle/1');
+
+        $crawler = $this->client->request('GET', '/battle/1');
         $buttonCrawlerNode = $crawler->selectButton('submit');
         $form = $buttonCrawlerNode->form();
         try {
-            $client->submit($form, [
+            $this->client->submit($form, [
                 'battle_form[myResult]' => '',
             ]);
         } catch (Exception $exception) {
@@ -47,34 +74,33 @@ class BattleControllerTest extends WebTestCase
 
     /**
      * @throws Exception
+     * @throws ORMException
+     * @throws OptimisticLockException
      */
     public function testCPUResult(): void
     {
         $today = new DateTime();
-        $client = static::createClient();
-        /** @var CpuResultLogRepository $cpuResultLogRepository */
-        $cpuResultLogRepository = static::$container->get(CpuResultLogRepository::class);
-        $cpuResultLogs = $cpuResultLogRepository->findResultByToday($today->format('Y-m-d'));
+        $cpuResultLogs = $this->cpuResultLogRepository->findResultByToday($today->format('Y-m-d'));
         if (!empty($cpuResultLogs)) {
             foreach ($cpuResultLogs as $cpuResultLog) {
-                $cpuResultLogRepository->remove($cpuResultLog);
+                $this->cpuResultLogRepository->remove($cpuResultLog);
             }
         }
         $cpuResultLog1 = new CpuResultLog();
         $cpuResultLog1->setResult(CpuResultLog::ID_SISSORS);
-        $cpuResultLogRepository->add($cpuResultLog1);
+        $this->cpuResultLogRepository->add($cpuResultLog1);
         $cpuResultLog2 = new CpuResultLog();
         $cpuResultLog2->setResult(CpuResultLog::ID_SISSORS);
-        $cpuResultLogRepository->add($cpuResultLog2);
+        $this->cpuResultLogRepository->add($cpuResultLog2);
         $cpuResultLog3 = new CpuResultLog();
         $cpuResultLog3->setResult(CpuResultLog::ID_ROCK);
-        $cpuResultLogRepository->add($cpuResultLog3);
+        $this->cpuResultLogRepository->add($cpuResultLog3);
         $cpuResultLog4 = new CpuResultLog();
         $cpuResultLog4->setResult(CpuResultLog::ID_PAPER);
-        $cpuResultLogRepository->add($cpuResultLog4);
+        $this->cpuResultLogRepository->add($cpuResultLog4);
 
-        $crawler = $client->request('GET', '/battle/1'); //userId 1 は10pt以上ないとダメ
-        $resultRate = $crawler->filterXpath('//p[3]')->text();
+        $crawler = $this->client->request('GET', '/battle/1');
+        $resultRate = $crawler->filter('#result')->text();
         $this->assertSame('グー：25％　パー：25％　チョキ：50％', $resultRate);
     }
 }
--- tests/LoginControllerTest.php
+++ tests/LoginControllerTest.php
@@ -3,37 +3,45 @@ namespace App\Tests;
 
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class LoginControllerTest extends WebTestCase
 {
+    /** @var KernelBrowser */
+    private $client;
+    /** @var UserRepository */
+    private $userRepository;
+
+    public function setUp()
+    {
+        $this->client = static::createClient();
+        $this->userRepository = static::$container->get(UserRepository::class);
+    }
+
     public function testFirstPoint(): void
     {
-        $client = static::createClient();
-        $userRepository = static::$container->get(UserRepository::class);
-        $crawler = $client->request('GET', '/login');
+        $crawler = $this->client->request('GET', '/login');
         $buttonCrawlerNode = $crawler->selectButton('submit');
         $form = $buttonCrawlerNode->form();
-        $client->submit($form, [
+        $this->client->submit($form, [
             'user_form[name]' => 'abc',
         ]);
         /** @var User $user */
-        $user = $userRepository->findOneBy(['name' => 'abc']);
+        $user = $this->userRepository->findOneBy(['name' => 'abc']);
         $this->assertEquals($user->getPoint(), 100);
     }
 
     public function testNullName(): void
     {
-        $client = static::createClient();
-        $userRepository = static::$container->get(UserRepository::class);
-        $crawler = $client->request('GET', '/login');
+        $crawler = $this->client->request('GET', '/login');
         $buttonCrawlerNode = $crawler->selectButton('submit');
         $form = $buttonCrawlerNode->form();
-        $client->submit($form, [
+        $this->client->submit($form, [
             'user_form[name]' => '',
         ]);
         /** @var User $user */
-        $user = $userRepository->findOneBy(['name' => '']);
+        $user = $this->userRepository->findOneBy(['name' => '']);
         $this->assertNull($user);
     }
 }
