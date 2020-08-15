93f95387f36115f9ae07270adad1bc0f3847d03a
Bravedragon <masa010515@hera.eonet.ne.jp>
2020-08-15 18:05:34 +0900

    テストコードを修正

--- tests/Service/HandServiceTest.php
+++ tests/Service/HandServiceTest.php
@@ -1,7 +1,6 @@
 <?php
 //参考サイト：https://symfony.com/doc/current/testing/database.html
 namespace App\Tests\Service;
-require_once __DIR__.'/../vender/autoload.php';
 use Symfony\Component\Finder;
 use App\Service\HandService;
 use DateTime;
